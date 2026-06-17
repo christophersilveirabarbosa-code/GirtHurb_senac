b9c38455a96d82bd5806e269ebda6f325e90d8b4
CHRISTOPHER BARBOSA SILVEIRA <christopher.9900@aluno.pr.senac.br>
2026-06-17 08:59:22 -0300

    first commit

---
+++ iMC.py
@@ -0,0 +1,21 @@
+# Programa para calcular IMC
+
+peso = float(input("Digite seu peso (kg): "))
+altura = float(input("Digite sua altura (m): "))
+
+imc = peso / (altura ** 2)
+
+print(f"\nSeu IMC é: {imc:.2f}")
+
+if imc < 18.5:
+    print("Classificação: Baixo peso")
+elif imc < 25:
+    print("Classificação: Peso normal")
+elif imc < 30:
+    print("Classificação: Sobrepeso")
+elif imc < 35:
+    print("Classificação: Obesidade grau I")
+elif imc < 40:
+    print("Classificação: Obesidade grau II")
+else:
+    print("Classificação: Obesidade grau III")
